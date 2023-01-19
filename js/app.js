23ebb5544bf42e53f67608ce01c5530c4110728d
Harrison <wilkinsnigel2000@gmail.com>
2023-01-19 12:27:51 -0800

    excellence

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-let selectText = document.getElementsByClassName('selectText');
 let Button1 = document.getElementById('Button1');
 let Button2 = document.getElementById('Button2');
 
+let titleText = document.getElementsByClassName('titleText');
+let selectText = document.getElementsByClassName('selectText');
+
 Button1.addEventListener('click', function () {
-    OnePlayer(params);
+    
 })
 Button2.addEventListener('click', function () {
     TwoPlayer(params);
